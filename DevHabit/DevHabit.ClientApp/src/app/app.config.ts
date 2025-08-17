4657a16b6344b338edcaaa9f4c4a1ed59f3e6721
vlad <wladik420@gmail.com>
2025-08-17 17:33:51 +0300

    Initialize angular client app and install ngxs,primeng

---
+++ DevHabit/DevHabit.ClientApp/src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideStore } from '@ngxs/store';
+import { environment } from '../environments/environment';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore([], {
+      developmentMode: !environment.production
+    }),
+    provideAnimationsAsync(),
+    providePrimeNG()
+  ]
+};
---
+++ DevHabit/DevHabit.ClientApp/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  authority: 'https://localhost:7260',
+  production: false,
+  buildNumber: '0.0.0',
+};
